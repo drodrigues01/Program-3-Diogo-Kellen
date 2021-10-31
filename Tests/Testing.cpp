929ef583b749338ef0f0fef9904c8b4954b538b3
drodrigues01 <drodrigues@smu.edu>
2021-10-31 16:53:16 -0500

    Updated buil.yaml

---
+++ Tests/Testing.cpp
@@ -0,0 +1,14 @@
+#include <boost/version.hpp>
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Testing if Boost is installed
+    std::cout << "Using Boost "
+    << BOOST_VERSION / 100000     << "."  // major version
+    << BOOST_VERSION / 100 % 1000 << "."  // minor version
+    << BOOST_VERSION % 100                // patch level
+    << std::endl;
+
+    
+}
