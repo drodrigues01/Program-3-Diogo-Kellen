f4f02c4990fa3cc7c3328e6cd756939537f8f9b5
drodrigues01 <drodrigues@smu.edu>
2021-11-01 09:31:08 -0500

    Studying how adjacency list works

--- Tests/Testing.cpp
+++ Tests/Testing.cpp
@@ -1,7 +1,11 @@
+// https://www.boost.org/doc/libs/1_37_0/libs/graph/doc/adjacency_list.html
+
 #include <boost/version.hpp>
 #include <iostream>
 #include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/adj_list_serialize.hpp"
 using namespace std;
+using namespace boost;
 
 int main(){
     //Testing if Boost is installed
@@ -11,6 +15,22 @@ int main(){
     << BOOST_VERSION % 100                // patch level
     << std::endl;
 
-    typedef adjacency_list, vecS> Graph;
+    typedef adjacency_list<listS, vecS, directedS,
+    no_property, property<edge_weight_t, int> > Graph;
+    typedef graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef std::pair<int,int> E;
 
+    const int num_nodes = 5;
+    E edges[] = { E(0,2),
+                  E(1,1), E(1,3), E(1,4),
+                  E(2,1), E(2,3),
+                  E(3,4),
+                  E(4,0), E(4,1) };
+    int weights[] = { 1, 2, 1, 2, 7, 3, 1, 1, 1};
+
+    Graph G(edges, edges + sizeof(edges) / sizeof(E), weights, num_nodes);
+
+
+
+    return 0;
 }
